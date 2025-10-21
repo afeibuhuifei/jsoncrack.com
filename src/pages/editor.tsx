03de149d506eb2a39df1cc714ef7557a08479219
afeibuhuifei <135005368+afeibuhuifei@users.noreply.github.com>
2025-10-21 13:01:14 +0800

    Update page title from 'JSON Crack' to 'CHU LONG FEI'

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -91,7 +91,7 @@ const EditorPage = () => {
     <>
       <NextSeo
         {...SEO}
-        title="Editor | JSON Crack"
+        title="Editor | CHU LONG FEI"
         description="JSON Crack Editor is a tool for visualizing into graphs, analyzing, editing, formatting, querying, transforming and validating JSON, CSV, YAML, XML, and more."
         canonical="https://jsoncrack.com/editor"
       />
